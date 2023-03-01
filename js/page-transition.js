431d8534ac1e75d5c168ff4a2c53f809a6b331dd
Lynn Wolters <lynn.wolters27@gmail.com>
2023-03-01 15:54:08 +0100

    v

---
+++ js/page-transition.js
@@ -0,0 +1,6 @@
+function changePage() {
+    document.body.classList.toggle('pagina-1');
+    document.body.classList.toggle('pagina-2');
+    document.body.classList.toggle('pagina-3');
+    document.body.classList.toggle('pagina-4');
+}
